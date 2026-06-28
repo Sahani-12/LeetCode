4620b2eebadc8842fc8cd58d11e2302c7c2be927
Anand Sahani <148997854+Sahani-12@users.noreply.github.com>
2026-06-28 20:57:56 +0530

    Time: 50 ms (17.78%), Space: 53.3 MB (74.39%) - LeetHub

--- 2635-apply-transform-over-each-element-in-array/2635-apply-transform-over-each-element-in-array.js
+++ 2635-apply-transform-over-each-element-in-array/2635-apply-transform-over-each-element-in-array.js
@@ -4,6 +4,9 @@
  * @return {number[]}
  */
 var map = function(arr, fn) {
-    return arr.map((arr,num)=>fn(arr,num))
-    
+    let res=[];
+    for(let i=0;i<arr.length;i++){
+        res.push(fn(arr[i],i))
+    }
+    return res;
 };
